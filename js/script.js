c992e86f09a81bdc2a8019d847ee5507bbe6c944
Your Name <you@example.com>
2025-03-17 19:12:13 +0100

    trabajo final

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 // 1 traer los datos de la API v
 // 2 Meter los datos traspasados del fetch dentro del DOM. v
 // 3 crear funciones necesarias  v
-// 4 configurar paginacion 
-// 5 agregar eventos a los botones de paginacion
+// 4 configurar paginacion v
+// 5 agregar eventos a los botones de paginacion v
 
 
  // 3 crear funciones necesarias, en este caso para mostrar los datos de cada personaje y agregarlos al DOM
@@ -15,19 +15,24 @@ function mostrarDatos(character) {
     };
     
 
-// 1 traer los datos de la API
-fetch(`https://rickandmortyapi.com/api/character/?page=1`)
+
+let currentPage = 1;
+let totalPages = 0;   
+//1 traer los datos de la API
+function loadCharacters(page) {
+ fetch(`https://rickandmortyapi.com/api/character/?page=${page}`)
     .then((response) => {
         if (!response.ok) {
             throw new Error('No se pudo realizar su solicitud');
         }
             return response.json();
         })
-
+        
     .then((data) => {
               //console.log(data)
         let ulCharactersList = document.getElementById('character-list'); // 2 Meter los datos traspasados del fetch dentro del DOM. (CAPTURE UL)
-        
+        ulCharactersList.innerHTML = ''; 
+
         data.results.forEach(character=> { //asumo que character tmb lo trae de la API
             let li = document.createElement('li'); // 2 Meter los datos traspasados del fetch dentro del DOM. (CREAR LI PARA CADA PERSONAJE, por eso dentro de f(x))
             let img = document.createElement('img'); // 2 Meter los datos traspasados del fetch dentro del DOM. (CREAR IMG PARA CADA PERSONAJE, por eso dentro de f(x))
@@ -38,12 +43,32 @@ fetch(`https://rickandmortyapi.com/api/character/?page=1`)
             
             li.innerHTML += mostrarDatos(character); // 2 Meter los datos traspasados del fetch dentro del DOM.(modifica li dentro de UL con el contenido de la f(x) mostrarDatos)
             ulCharactersList.appendChild(li); // 2 Meter los datos traspasados del fetch dentro del DOM. (AGREGAR LI A UL)
-        });
+            totalPages = data.info.pages; 
+         //console.log(data.info.pages); // 2 Meter los datos traspasados del fetch dentro del DOM. (mostrar en consola el numero de paginas)
+        })
         
     })  
     .catch((error) => {
         console.log('Error:', error);
     });
-
-  
+}
    
+// 5 agregar eventos a los botones de paginacion
+
+loadCharacters(currentPage);
+const prevPage = document.getElementById('prev-page'); 
+const nextPage = document.getElementById('next-page');  
+
+prevPage.addEventListener('click', () => {
+    if (currentPage > 1) {
+        currentPage--;
+        loadCharacters(currentPage);
+    }
+});
+
+nextPage.addEventListener('click', () => {
+   if (currentPage < totalPages) {
+        currentPage ++;
+        loadCharacters(currentPage);
+   }
+    });
